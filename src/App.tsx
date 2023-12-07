9133d2c8ce532d5e3299dc4035df0e5965c5c489
Felipe Almeida <almeida.felipe98@gmail.com>
2023-12-06 21:02:11 -0300

    structure search bar

--- src/App.tsx
+++ src/App.tsx
@@ -19,11 +19,13 @@ import "@ionic/react/css/display.css";
 /* Theme variables */
 import "./theme/variables.css";
 import Tabs from "./components/Tabs/Tabs";
+import SearchBar from "./components/SearchBar/SearchBar";
 
 setupIonicReact();
 
 const App: React.FC = () => (
   <IonApp>
+    <SearchBar />
     <Tabs />
   </IonApp>
 );
--- src/pages/Apps/Apps.tsx
+++ src/pages/Apps/Apps.tsx
@@ -1,10 +1,12 @@
 import { IonContent, IonPage } from "@ionic/react";
 import ExploreContainer from "../../components/ExploreContainer";
+import SearchBar from "../../components/SearchBar/SearchBar";
 
 const Apps: React.FC = () => {
   return (
     <IonPage>
       <IonContent>
+        <SearchBar />
         <ExploreContainer name="Apps page" />
       </IonContent>
     </IonPage>
