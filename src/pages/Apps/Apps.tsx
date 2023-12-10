1c5508e56807e785e3d470f1e83bd39fa20c0dfb
gamleal <guilhermeizidroleal@gmail.com>
2023-12-10 16:32:18 -0300

    Adicionada pagina de jogos/apps/livros

--- src/pages/Apps/Apps.tsx
+++ src/pages/Apps/Apps.tsx
@@ -1,4 +1,4 @@
-import { IonContent, IonPage,useIonViewWillEnter } from "@ionic/react";
+import { IonContent, IonPage, useIonViewWillEnter, IonGrid, IonRow, IonCol } from "@ionic/react";
 import ExploreContainer from "../../components/ExploreContainer";
 
 const Apps: React.FC = () => {
@@ -11,7 +11,86 @@ const Apps: React.FC = () => {
   return (
     <IonPage>
       <IonContent>
-        <ExploreContainer name="Apps page" />
+        <IonGrid>
+          <IonRow className="row-class">
+            <IonCol className="column-class column-class-first">Para você</IonCol>
+            <IonCol className="column-class">Em alta</IonCol>
+            <IonCol className="column-class">Novidades</IonCol>
+            <IonCol className="column-class">Premium</IonCol>
+          </IonRow>
+          {/* Banner1 */}
+          <div className="main-item-banner">
+            <a href="#">
+              <img className="banner-img" src="/duo_banner.png" />
+            </a>
+            <div className="banner-content">
+              <IonCol size="2">
+                <a href="#">
+                  <img className="icon-img" src="/duo_icon.png" />
+                </a>
+              </IonCol>
+              <IonCol size="8">
+                <p className="item-line-one">Duolingo</p>
+                <div>
+                  <p className="item-line-two">Educação • 4,7 <img className="rating-star" src="/full-star.svg" /></p>
+                </div>
+              </IonCol>
+              <IonCol size="2">
+                <a href="#">
+                  <img className="download-img" src="/download-icon.svg" />
+                </a>
+              </IonCol>
+            </div>
+          </div>
+          {/* Banner2 */}
+          <div className="main-item-banner">
+            <a href="#">
+              <img className="banner-img" src="/duo_banner.png" />
+            </a>
+            <div className="banner-content">
+              <IonCol size="2">
+                <a href="#">
+                  <img className="icon-img" src="/duo_icon.png" />
+                </a>
+              </IonCol>
+              <IonCol size="8">
+                <p className="item-line-one">Duolingo</p>
+                <div>
+                  <p className="item-line-two">Educação • 4,7 <img className="rating-star" src="/full-star.svg" /></p>
+                </div>
+              </IonCol>
+              <IonCol size="2">
+                <a href="#">
+                  <img className="download-img" src="/download-icon.svg" />
+                </a>
+              </IonCol>
+            </div>
+          </div>
+          {/* Banner3 */}
+          <div className="main-item-banner">
+            <a href="#">
+              <img className="banner-img" src="/duo_banner.png" />
+            </a>
+            <div className="banner-content">
+              <IonCol size="2">
+                <a href="#">
+                  <img className="icon-img" src="/duo_icon.png" />
+                </a>
+              </IonCol>
+              <IonCol size="8">
+                <p className="item-line-one">Duolingo</p>
+                <div>
+                  <p className="item-line-two">Educação • 4,7 <img className="rating-star" src="/full-star.svg" /></p>
+                </div>
+              </IonCol>
+              <IonCol size="2">
+                <a href="#">
+                  <img className="download-img" src="/download-icon.svg" />
+                </a>
+              </IonCol>
+            </div>
+          </div>
+        </IonGrid>
       </IonContent>
     </IonPage>
   );
--- src/pages/Games/Games.tsx
+++ src/pages/Games/Games.tsx
@@ -1,11 +1,97 @@
-import { IonContent, IonPage } from "@ionic/react";
+import { IonContent, IonPage, useIonViewWillEnter, IonGrid, IonRow, IonCol } from "@ionic/react";
 import ExploreContainer from "../../components/ExploreContainer";
+import './Games.css';
 
 const Games: React.FC = () => {
+  useIonViewWillEnter(() => {
+    const tabBar = document.querySelector('ion-tab-bar');
+    if (tabBar) {
+      tabBar.style.display = 'flex';
+    }
+  }, []);
   return (
     <IonPage>
       <IonContent>
-        <ExploreContainer name="Games page" />
+        <IonGrid>
+          <IonRow className="row-class">
+            <IonCol className="column-class column-class-first">Para você</IonCol>
+            <IonCol className="column-class">Em alta</IonCol>
+            <IonCol className="column-class">Novidades</IonCol>
+            <IonCol className="column-class">Premium</IonCol>
+          </IonRow>
+          {/* Banner1 */}
+          <div className="main-item-banner">
+            <a href="#">
+              <img className="banner-img" src="/duo_banner.png" />
+            </a>
+            <div className="banner-content">
+              <IonCol size="2">
+                <a href="#">
+                  <img className="icon-img" src="/duo_icon.png" />
+                </a>
+              </IonCol>
+              <IonCol size="8">
+                <p className="item-line-one">Duolingo</p>
+                <div>
+                  <p className="item-line-two">Educação • 4,7 <img className="rating-star" src="/full-star.svg" /></p>
+                </div>
+              </IonCol>
+              <IonCol size="2">
+                <a href="#">
+                  <img className="download-img" src="/download-icon.svg" />
+                </a>
+              </IonCol>
+            </div>
+          </div>
+          {/* Banner2 */}
+          <div className="main-item-banner">
+            <a href="#">
+              <img className="banner-img" src="/duo_banner.png" />
+            </a>
+            <div className="banner-content">
+              <IonCol size="2">
+                <a href="#">
+                  <img className="icon-img" src="/duo_icon.png" />
+                </a>
+              </IonCol>
+              <IonCol size="8">
+                <p className="item-line-one">Duolingo</p>
+                <div>
+                  <p className="item-line-two">Educação • 4,7 <img className="rating-star" src="/full-star.svg" /></p>
+                </div>
+              </IonCol>
+              <IonCol size="2">
+                <a href="#">
+                  <img className="download-img" src="/download-icon.svg" />
+                </a>
+              </IonCol>
+            </div>
+          </div>
+          {/* Banner3 */}
+          <div className="main-item-banner">
+            <a href="#">
+              <img className="banner-img" src="/duo_banner.png" />
+            </a>
+            <div className="banner-content">
+              <IonCol size="2">
+                <a href="#">
+                  <img className="icon-img" src="/duo_icon.png" />
+                </a>
+              </IonCol>
+              <IonCol size="8">
+                <p className="item-line-one">Duolingo</p>
+                <div>
+                  <p className="item-line-two">Educação • 4,7 <img className="rating-star" src="/full-star.svg" /></p>
+                </div>
+              </IonCol>
+              <IonCol size="2">
+                <a href="#">
+                  <img className="download-img" src="/download-icon.svg" />
+                </a>
+              </IonCol>
+            </div>
+          </div>
+        </IonGrid>
       </IonContent>
     </IonPage>
   );
